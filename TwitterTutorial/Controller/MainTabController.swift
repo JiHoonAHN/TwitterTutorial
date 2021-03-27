0acfd40900f7caf3d8434aedc39d22954b0f5136
JiHoonAHN <ahnjh1028@naver.com>
2021-03-28 01:30:24 +0900

    Pushing

---
+++ TwitterTutorial/Controller/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  TwitterTutorial
+//
+//  Created by Ji-hoon Ahn on 2021/03/28.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+
+}
